5d71cde6ac75461ea89827a70dfd0a5bcc0fff8c
Alí Sinisterra <haidarconh@gmail.com>
2025-08-14 18:50:58 -0500

    add mock API

---
+++ api.js
@@ -0,0 +1,12 @@
+const getPuzzleForToday = () => {
+    // TODO: add backend
+    return {
+        date: new Date().toISOString(),
+        snippets: [
+            'So she was considering in her own mind (as well as she could, for the {hot} day made her feel very sleepy and stupid)',
+            "To be sure, it might be nothing but a good coat of tropical tanning; but I never heard of a {hot} sun's tanning a white man into a purplish yellow one.",
+            "before Wyclif's days remained lawful and were in some folks' hands, yet he thought no printer would likely be so {hot} to put any Bible in print at his own charge,",
+        ],
+        word: 'hot',
+    }
+}
