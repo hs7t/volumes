60991d2d9c8aa240c329d8f5ae42b4451e48f367
Alí Sinisterra <haidarconh@gmail.com>
2025-08-19 16:30:23 -0500

    Add date to game screen

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -3,10 +3,15 @@
   import Snippets from './lib/Snippets.svelte'
   import StatusBar from './lib/StatusBar.svelte'
   import Answers from './lib/Answers.svelte'
+  
+  let date = new Date
 </script>
 
 <main>
-  
+  <div>
+    <h1>Volumes</h1>
+    <p>{date.toDateString()}</p>
+  </div>
   <StatusBar />
   <Snippets {snippets}/>
   <Answers />
