5a14d628b458d3a2bfdd11f80848b7d2bee65a93
Alí Sinisterra <haidarconh@gmail.com>
2025-08-19 15:17:05 -0500

    update app; add test code

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,9 +1,20 @@
 <script>
   import Snippets from './lib/Snippets.svelte'
+  import StatusBar from './lib/StatusBar.svelte'
+
+  let score = $state(0)
 </script>
 
 <main>
-  <Snippets></Snippets>
+  <StatusBar {score} />
+
+  <Snippets snippets={[
+    {
+      content: "hello",
+      source: "Alice in Wonderland",
+      date: "2000"
+    }
+  ]} />
 </main>
 
 <style>
