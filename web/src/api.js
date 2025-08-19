75240de8e47a15549dda6411a307b01fd6bcc918
Alí Sinisterra <haidarconh@gmail.com>
2025-08-19 16:12:44 -0500

    add a mock API

---
+++ web/src/api.js
@@ -0,0 +1,9 @@
+const fetchSnippets = () => {
+    return [
+        {
+            content: 'hello, world, i am {tired}',
+            source: 'Alice in Wonderland',
+            date: '2000',
+        },
+    ]
+}
