b36c981dc34ee48ed1475788f61645d9f336e45a
Alí Sinisterra <haidarconh@gmail.com>
2025-08-19 15:16:17 -0500

    add snippet data to Snippets component

--- web/src/lib/Snippets.svelte
+++ web/src/lib/Snippets.svelte
@@ -6,6 +6,14 @@
     <h1>Snippets</h1>
     <div class="snippets">
         {#each snippets as snippet}
+            <figure class="snippet">
+                <blockquote>
+                    {snippet.content.replace(/{.*?}/gm, '[__]')}
+                </blockquote>
+                <figcaption>
+                    From <cite>{snippet.source}</cite>, {snippet.date}
+                </figcaption>
+            </figure>
         {/each}
     </div>
 </div>
