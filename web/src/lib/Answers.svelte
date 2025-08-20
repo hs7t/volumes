8f049ad15c73d48163dc4159b7990f85a0014050
Alí Sinisterra <haidarconh@gmail.com>
2025-08-19 21:32:22 -0500

    start making Answers functional

--- web/src/lib/Answers.svelte
+++ web/src/lib/Answers.svelte
@@ -1,22 +1,27 @@
 <script>
-    let { input = "", guesses = [""] } = $props()
+    import { score } from '../shared.svelte.js'
+
+    let guesses = $state([])
+    let input = $state("")
 
     const processSubmit = (event) => {
-        
+        guesses.push(input)
+        $score -= 1
     }
 </script>
 
 <div class="card-area">
     <h2>What's today's word?</h2>
-    <form onsubmit={processSubmit}>
+    <div id="answer-form">
         <div class="input">
             <label for="input">Type in a guess...</label>
-            <input type="text" id="input">
+            <input type="text" id="input" bind:value={input}>
         </div>
-
-        <button>This is it!</button>
-    </form>
+        <button onclick={processSubmit}>This is it!</button>
+    </div>
+    {#if (guesses.length > 0)}
     <p class="guess-reminder">Your guesses: {#each guesses as guess}<span class="guess">{guess}</span>{/each}</p>
+    {/if}
 </div>
 
 <style>
@@ -27,5 +32,6 @@
 
     .guess-reminder .guess {
         text-decoration: line-through;
+        margin-right: 1ch;
     }
 </style>
