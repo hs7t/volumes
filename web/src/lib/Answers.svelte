6df349c935f1869b901239357574dda4a05ba1f2
Alí Sinisterra <haidarconh@gmail.com>
2025-08-20 20:29:43 -0500

    move TextInput to own component

--- web/src/lib/Answers.svelte
+++ web/src/lib/Answers.svelte
@@ -1,5 +1,6 @@
 <script>
     import { score } from '../shared.svelte.js'
+    import TextInput from './components/TextInput.svelte';
 
     let guesses = $state([])
     let input = $state("")
@@ -12,24 +13,13 @@
 
 <div class="card-area">
     <h2>What's today's word?</h2>
-    <div id="answer-form">
-        <div class="input">
-            <label for="input">Type in a guess...</label>
-            <input type="text" id="input" bind:value={input}>
-        </div>
-        <button onclick={processSubmit}>This is it!</button>
-    </div>
+    <TextInput bind:input={input} action={processSubmit}></TextInput>
     {#if (guesses.length > 0)}
     <p class="guess-reminder">Your guesses: {#each guesses as guess}<span class="guess">{guess}</span>{/each}</p>
     {/if}
 </div>
 
 <style>
-    .input {
-        display: inline-flex;
-        gap: 1ch;
-    }
-
     .guess-reminder .guess {
         text-decoration: line-through;
         margin-right: 1ch;
---
+++ web/src/lib/components/TextInput.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Button from "./Button.svelte";
+
+    let { action, input = $bindable(), inputLabel = undefined, buttonLabel = "Enter" } = $props()
+</script>
+
+<div class="answer-form">
+    <div class="input">
+        {#if inputLabel}
+        <label for="input">{inputLabel}</label>
+        {/if}
+        <input type="text" id="input" bind:value={input}>
+    </div>
+    <Button {action} text={buttonLabel}></Button>
+</div>
+
+<style>
+    .input {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
