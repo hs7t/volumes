4e4031c70958cd4ecbb6e976fc6df73e7ccfbf74
Alí Sinisterra <haidarconh@gmail.com>
2025-08-20 21:09:55 -0500

    make buttons prettier

--- web/src/lib/components/Button.svelte
+++ web/src/lib/components/Button.svelte
@@ -14,5 +14,10 @@
         max-width: 100%;
         padding: 0.8ch;
         font-family: var(--v-font-secondary);
+
+        background-color: var(--v-color-accent);
+        color: var(--v-color-accentContrast);
+        border-radius: var(--v-radius-secondary);
+        border: none;
     }
 </style>
