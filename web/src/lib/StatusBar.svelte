2c30e9cd267c646fd0ba77f75878abcd79d18c52
Alí Sinisterra <haidarconh@gmail.com>
2025-08-21 19:41:45 -0500

    make score count nicer

--- web/src/lib/StatusBar.svelte
+++ web/src/lib/StatusBar.svelte
@@ -3,5 +3,13 @@
 </script>
 
 <div class="card-area">
-    <p>Score: {$score}</p>
+    <p class="score-count">Score: {$score}</p>
 </div>
+
+<style>
+    @import '../style/shared.css';
+
+    .score-count {
+        font-family: var(--v-font-secondary);
+    }
+</style>
