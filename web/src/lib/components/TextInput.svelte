b5cb004c2075f719745f3018e5d464249e1b1f71
Alí Sinisterra <haidarconh@gmail.com>
2025-08-21 19:46:33 -0500

    input box styling !!!

--- web/src/lib/components/TextInput.svelte
+++ web/src/lib/components/TextInput.svelte
@@ -7,9 +7,9 @@
 <div class="answer-form">
     <div class="input">
         {#if inputLabel}
-        <label for="input">{inputLabel}</label>
+        <label for="input-box">{inputLabel}</label>
         {/if}
-        <input type="text" id="input" bind:value={input}>
+        <input type="text" class="input-box" bind:value={input}>
     </div>
 
     {#if action}
@@ -29,6 +29,22 @@
     .input {
         display: flex;
         flex-direction: column;
+    }
+
+    .input-box {
+        font-size: var(--v-size-font-primary);
+        font-family: var(--v-font-secondary);
+        padding: 0.8ch;
+
+        border: var(--v-border-primary);
+        border-radius: var(--v-radius-secondary);
+        background-color: var(--v-color-backgroundC);
+        color: inherit;
+    }
+
+    .input-box:hover, .input-box:active, .input-box:focus {
+        transition: all 200ms;
+        border: var(--v-border-primaryThick);
     }
 
     label {
