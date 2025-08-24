cd335f3d9bbeca7deffc3ffbf0100f8c4b709a87
Alí Sinisterra <haidarconh@gmail.com>
2025-08-24 15:02:12 -0500

    update api.js ! new function

--- web/src/api.js
+++ web/src/api.js
@@ -1,16 +1,23 @@
 export const fetchSnippets = () => {
-    return [
-        {
-            content: 'I bet on {losing} dogs',
-            source: '"Puberty"',
-            by: 'Mitski',
-            date: '2016',
-        },
-        {
-            content: "I'm {losing} my temper",
-            source: 'Trust me bro',
-            by: 'Me',
-            date: '2025',
-        },
-    ]
+    return fetchPuzzle().snippets
+}
+
+export const fetchPuzzle = () => {
+    return {
+        snippets: [
+            {
+                content: 'I bet on {losing} dogs',
+                source: '"Puberty"',
+                by: 'Mitski',
+                date: '2016',
+            },
+            {
+                content: "I'm {losing} my temper",
+                source: 'Trust me bro',
+                by: 'Me',
+                date: '2025',
+            },
+        ],
+        solution: 'losing',
+    }
 }
