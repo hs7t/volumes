1580488e69e5c1cb1d218d0ca0e82c2b92a22554
Alí Sinisterra <haidarconh@gmail.com>
2025-08-24 18:27:58 -0500

    start work on end screen

---
+++ web/src/lib/EndScreen.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Dialog from "./components/Dialog.svelte"
+
+    let { shown, won } = $props()
+    let dialogContent = {}
+    switch (won) {
+        case true:
+            dialogContent.title = "Woohoo!"
+            dialogContent.message = "You won!"
+            break
+        case false:
+            dialogContent.title = "Good game!"
+            dialogContent.message = "Ah, you didn't make it this time. It's alright, though."
+    }
+</script>
+
+{#snippet content()}
+    <h2>Good game!</h2>
+{/snippet}
+
+<Dialog {content} bind:shown={shown}></Dialog>
