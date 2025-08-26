1a3cb7ed6120025ce6eb870c23f5bc12de0101b7
Alí Sinisterra <haidarconh@gmail.com>
2025-08-25 20:07:24 -0500

    work on EndScreen

--- web/src/lib/EndScreen.svelte
+++ web/src/lib/EndScreen.svelte
@@ -1,38 +1,41 @@
 <script>
     import Dialog from "./components/Dialog.svelte"
-    import { gameState, checkStatus, fetchShownHints } from "../shared.svelte.js"
+    import { gameState, checkStatus, fetchShownHints, resetGame } from "../shared.svelte.js"
+    import Button from "./components/Button.svelte"
     
     let gameStatus = $derived(checkStatus(gameState))
     let shown = $derived(gameStatus == 'won' || gameStatus == 'lost')
-    let dialogContent = {
+    let dialogContent = $state({
+        title: "",
+        message: "",
         stats:  {
             score: gameState.score,
             hintAmount: fetchShownHints().length,
         }
-    }
+    })
 
     $effect(() => {
         switch (gameStatus) {
             case 'won':
-                dialogContent.title = "Woohoo!"
-                dialogContent.message = "You won!"
+                dialogContent["title"] = "Woohoo!"
+                dialogContent["message"] = "You won!"
                 break
             case 'lost':
-                dialogContent.title = "Good game!"
-                dialogContent.message = "Ah, you didn't make it this time. It's alright, though."
-                dialogContent.hint = ("The word was " + gameState.solution)
+                dialogContent["title"] = "Good game!"
+                dialogContent["message"] = "Ah, you didn't make it this time. It's alright, though."
+                dialogContent["hint"] = ("The word was " + gameState.solution)
                 break
         }
     })
 </script>
 
 {#snippet content()}
-    <h2>{dialogContent.title}</h2>
+    <h2>{dialogContent?.title}</h2>
     <p>
-        {dialogContent.message}. Your score was of {dialogContent.stats.score}, and you 
-        used up {dialogContent.stats.hintAmount} snippet
-        {#if (!(dialogContent.stats.hintAmount == 1))}s{:else}{/if}.
+        {dialogContent?.message} Your score was of {dialogContent?.stats?.score}, and you 
+        used {dialogContent.stats.hintAmount} snippet{#if (!(dialogContent.stats.hintAmount == 1))}s{:else}{/if}.
     </p>
+    <Button action={resetGame} text="Solve again" />
 {/snippet}
 
 <Dialog {content} bind:shown={shown}></Dialog>
