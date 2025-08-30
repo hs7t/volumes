a206c1122840bccf2b6c8e1e2b1c3be87432991d
Alí Sinisterra <haidarconh@gmail.com>
2025-08-30 17:21:42 -0500

    fix API implementation

--- src/api.js
+++ src/api.js
@@ -1,12 +1,18 @@
 async function fromJSONFile(path) {
-    const file = (await fetch(path)).json()
-    return JSON.parse(await file)
+    const response = await fetch(path)
+
+    const text = await response.text()
+    console.log('Response:', text) // See what you're actually getting
+    console.log('Status:', response.status)
+    console.log('URL:', response.url)
+
+    return JSON.parse(text)
 }
 
-const config = fromJSONFile('./config.json')
-const apiURL = config['volnet_api_url']
+const config = await fromJSONFile(`${import.meta.env.BASE_URL}config.json`)
+const apiURL = await config['volnet_api_url']
 
-export const fetchPuzzle = () => {
-    const puzzleURL = new URL('/puzzles/', apiURL)
+export async function fetchPuzzle() {
+    const puzzleURL = new URL('v1/puzzles/latest', apiURL)
     return fromJSONFile(puzzleURL)
 }
